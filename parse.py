4d586246103796118f3c9c0764ece656258fea84
chris <chris@chrphb.com>
2021-03-07 15:07:53 +0100

    improve parsing

--- parse.py
+++ parse.py
@@ -1,5 +1,14 @@
+def special_parse(line, second_separator):
+    s1 = line.find(" ")
+    rest = line[s1+1:]
+    s2 = rest.find(second_separator)
+    h = line[:s1]
+    p = rest[0:s2]
+    f = rest[s2+1:]
+    return (h,p,f.strip())    
+
+
 print("start")
-sum = 0
 total = 0
 with open("/home/chris/brain/1-projects/python-hsk2/data/raw.txt",'r')  as reader:
     for line in reader.readlines():
@@ -8,13 +17,15 @@ with open("/home/chris/brain/1-projects/python-hsk2/data/raw.txt",'r')  as reade
             # find the first space
             s = line.find(' ')
             notraditional = line[s+1:]
-            print(line,end='')
-            print(notraditional, end='')
-            c = notraditional.count(' ')
-            print(c)
+            #print(line,end='')
+            #print(notraditional, end='')
+            if(line.find("#")!=-1):
+                r = special_parse(notraditional, "#")
+                print(r)
+            else:
+                r = special_parse(notraditional, " ")
+                print(r)
             total = total + 1
-            if(c==2):
-                sum = sum + 1
     print("ok")
 print("end")
-print(sum,"out of",total)
+print("out of",total)
